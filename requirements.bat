baab32103dcbffbe3d51879ddfcdbfa6468c091c
mariodac <mdac.mario@gmail.com>
2022-05-30 15:57:52 -0300

    criação de pasta uploads

--- requirements.bat
+++ requirements.bat
@@ -1,3 +1,4 @@
 REM executar script dentro da pasta 
 py -3 -m venv venv
 venv\Scripts\pip install -r requirements.txt
+mkdir project/static/uploads
--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,4 @@
 #executar script dentro da pasta 
 python3 -m venv venv
 venv/bin/pip install -r requirements.txt
+mkdir project/static/uploads
