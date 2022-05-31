707d727c38dd48bd869ca5bb99532bf977f69e10
mariodac <mdac.mario@gmail.com>
2022-05-31 15:51:04 -0300

    atualizados scripts sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
-#executar script dentro da pasta 
-python3 -m venv venv
-venv/bin/pip install -r requirements.txt
-mkdir project/static/uploads
-chmod +x run.sh
+#executar script dentro da pasta
+python3 -m venv /opt/auth_upload_files-main/venv
+/opt/auth_upload_files-main/venv/bin/pip install -r requirements.txt
+mkdir /opt/auth_upload_files-main/project/static/uploads
+chmod +x /opt/auth_upload_files-main/run.sh
