bbc97c44f073cef842aa550bf27e2d70f1851ba0
mariodac <mdac.mario@gmail.com>
2022-05-31 13:33:32 -0300

    Logs

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,9 @@
+from fileinput import filename
 from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from werkzeug.utils import secure_filename
+import logging 
 import imghdr
 import os
 
@@ -9,7 +11,20 @@ import os
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logger = logging.getLogger(__name__)
+# To override the default severity of logging
+logger.setLevel('DEBUG')
+
+# Use FileHandler() to log to a file
+file_handler = logging.FileHandler("info.log")
+formatter = logging.Formatter(log_format)
+file_handler.setFormatter(formatter)
+
+# Don't forget to add the file handler
+logger.addHandler(file_handler)
 def create_app():
+    logging.basicConfig(filename='debug.log',level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'secret-key-goes-here'
@@ -30,6 +45,7 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
+        logger.info('User %s loaded', user_id)
         return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
@@ -45,12 +61,14 @@ def create_app():
         stream.seek(0)
         format = imghdr.what(None, header)
         if not format:
+            logger.info('File with no format')
             print("not format")
             return None
         return '.' + (format if format != 'jpeg' else 'jpg')
 
     @app.errorhandler(413)
     def too_large(e):
+        logger.info('File is too large')
         return "File is too large", 413
 
     @app.route('/uploads', methods=['POST'])
@@ -62,9 +80,10 @@ def create_app():
             file_ext = os.path.splitext(filename)[1]
             if file_ext not in app.config['UPLOAD_EXTENSIONS']:
                 return "Invalid image", 400
-            uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+            save_file = os.path.join(app.config['UPLOAD_PATH'], filename)
+            uploaded_file.save(save_file)
+            logger.info('File save in %s',save_file)
         # print('arquivo enviado')
-        # flash('You were successfully logged in')
         return '', 204
 
     @app.route('/uploads/<filename>')
--- project/auth.py
+++ project/auth.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, escape, render_template, redirect, session, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
+from . import logger
 
 auth = Blueprint('auth', __name__)
 
@@ -19,6 +20,7 @@ def signup():
 @login_required
 def logout():
     logout_user()
+    logger.info('User %s logout', escape(session['username']))
     return redirect(url_for('main.index'))
 
 @auth.route('/signup', methods=['POST'])
@@ -30,16 +32,17 @@ def signup_post():
     user = User.query.filter_by(username=username).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
-        flash('Email address already exists')
+        flash('Username %s already exists', username)
+        logger.info('Username %s already exists', username)
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
     new_user = User(username=username, name=name, password=generate_password_hash(password, method='sha256'))
-
+    
     # add the new user to the database
     db.session.add(new_user)
     db.session.commit()
-
+    logger.info('New user %s registred', username)
     return redirect(url_for('auth.login'))
 
 @auth.route('/login', methods=['POST'])
@@ -47,16 +50,19 @@ def login_post():
     username = request.form.get('username')
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
-    print(remember)
+    # print(remember)
     user = User.query.filter_by(username=username).first()
 
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
+        logger.info('Error login with user %s', username)
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
     # if the above check passes, then we know the user has the right credentials
+    session['username'] = username
     login_user(user, remember=remember)
+    logger.info('Login with user %s', username)
     return redirect(url_for('main.profile'))
 
