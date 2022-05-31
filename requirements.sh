29e314e2ae2890bb9931fe078155ac9d2c50f6e7
mariodac <mdac.mario@gmail.com>
2022-05-31 16:03:26 -0300

    não aguento mais mexer nesse sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 #executar script dentro da pasta
-cd cd /opt/auth_upload_files-main/
+cd /opt/auth_upload_files-main/
 python3 -m venv venv
 venv/bin/pip install -r requirements.txt
 mkdir project/static/uploads
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
+cd /opt/auth_upload_files-main/
 export FLASK_APP=project
 export FLASK_DEBUG=1
 port=8085
 host_ip="127.0.0.1"
-. /opt/auth_upload_files-main/venv/bin/activate
+. venv/bin/activate
 flask run --port="$port" --host="$host_ip"
