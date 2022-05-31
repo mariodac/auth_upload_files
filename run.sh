0934e39dfe1746441d5e8267f6fd9046cfd339b9
mariodac <mdac.mario@gmail.com>
2022-05-31 16:06:16 -0300

    lá vamos nos outra vez

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export FLASK_APP=project
 export FLASK_DEBUG=1
 port=8085
 host_ip="127.0.0.1"
-. venv/bin/activate
+. /opt/auth_upload_files-main/venv/bin/activate
 flask run --port="$port" --host="$host_ip"
