66f354e5ac2af544d4f1513f4771dfc2979068f3
mariodac <38120268+mariodac@users.noreply.github.com>
2022-06-18 15:06:01 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ cd /opt/auth_upload_files-main/
 export FLASK_APP=project
 export FLASK_DEBUG=1
 port=8085
+#altere para 0.0.0.0 para acessar de qualquer dispositivo que esteja na sua rede
 host_ip="127.0.0.1"
 . /opt/auth_upload_files-main/venv/bin/activate
 flask run --port="$port" --host="$host_ip"
