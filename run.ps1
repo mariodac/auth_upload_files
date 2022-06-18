a3102128bc6f27a0f8a7127d3b145fffff2dc0b6
mariodac <38120268+mariodac@users.noreply.github.com>
2022-06-18 15:04:28 -0300

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,6 +1,7 @@
 $env:FLASK_APP = "project"
 $env:FLASK_DEBUG=1
 $port=8085
-$host_ip="127.0.0.1"
+#altere para 0.0.0.0 para acessar de qualquer dispositivo que esteja na sua rede
+$host_ip="127.0.0.1" 
 .\venv\Scripts\activate
 flask run  --host=$host_ip --port=$port
