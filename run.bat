f763edbae5a4638d1a77c9a02d24717fba9ccc78
mariodac <38120268+mariodac@users.noreply.github.com>
2022-06-18 15:05:27 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 set FLASK_APP=project
 set FLASK_DEBUG=1
 set port=8085
+:: altere para 0.0.0.0 para acessar de qualquer dispositivo na mesma rede
 set host_ip="127.0.0.1"
 .\venv\Scripts\activate && flask run --port=%port% --host=%host_ip%
